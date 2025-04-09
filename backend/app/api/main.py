55ed151db1efe4b325125a813d2e1378aefe29ed
Maxim Platonov <bliamba2000@gmail.com>
2025-04-09 20:30:12 +0400

    add utils and endpoint health-check

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.routes import password
+from app.api.routes import password, utils
 
 api_router = APIRouter()
 
-api_router.include_router(password.router, prefix="/password", tags=["password"])
+api_router.include_router(
+    password.router, prefix="/password", tags=["password"]
+)
+api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
